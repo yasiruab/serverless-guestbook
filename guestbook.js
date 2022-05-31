76af52773b2ec672c0bab9dd375ea602da6234a0
yasiruab <87564827+yasiruab@users.noreply.github.com>
2022-05-31 20:42:09 +0530

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://443c4088.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
